60fbaef4aea9023b355b48de3ffacc858bbe17af
BoiDG <alex.neil1011@gmail.com>
2023-08-18 21:46:57 +0700

    remove key

--- Google_ReCaptcha_MVC_Core/Models/RECaptcha.cs
+++ Google_ReCaptcha_MVC_Core/Models/RECaptcha.cs
@@ -7,9 +7,9 @@ namespace Google_ReCaptcha_MVC_Core.Models
 {
     public class RECaptcha
     {
-        public string Key = "6LdG6LYnAAAAAEIaWbcsRpgkPwv1CniXDrFs06df";
+        public string Key = "";
 
-        public string Secret = "6LdG6LYnAAAAACIny5BwDKFRFqX0uDNtv1eJbN1L";
+        public string Secret = "";
         public string Response { get; set; }
     }
 
