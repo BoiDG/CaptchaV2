126ac5f019ab606c618188594f31535197e4107d
BoiDG <alex.neil1011@gmail.com>
2023-08-18 17:38:51 +0700

    Add project files.

---
+++ Google_ReCaptcha_MVC_Core/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Google_ReCaptcha_MVC_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Google_ReCaptcha_MVC_Core
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new RECaptcha());
+        }
+
+        [HttpPost]
+        public JsonResult AjaxMethod(string response)
+        {
+            RECaptcha recaptcha = new RECaptcha();
+            string url = "https://www.google.com/recaptcha/api/siteverify?secret=" + recaptcha.Secret + "&response=" + response;
+            recaptcha.Response = (new WebClient()).DownloadString(url);
+            return Json(recaptcha);
+        }
+    }
+}
---
+++ Google_ReCaptcha_MVC_Core/Models/RECaptcha.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Google_ReCaptcha_MVC_Core.Models
+{
+    public class RECaptcha
+    {
+        public string Key = "6LdG6LYnAAAAAEIaWbcsRpgkPwv1CniXDrFs06df";
+
+        public string Secret = "6LdG6LYnAAAAACIny5BwDKFRFqX0uDNtv1eJbN1L";
+        public string Response { get; set; }
+    }
+
+}
